57dabdc6461ac75fb6f103f0d9e97593114b0ddb
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-04-03 21:35:59 +0530

    Operations on SLL

--- TopicwiseCodingPractice/LinkedList/InsertSearchDelete/SingleLinkedListOperations.cpp
+++ TopicwiseCodingPractice/LinkedList/InsertSearchDelete/SingleLinkedListOperations.cpp
@@ -154,7 +154,7 @@ int main()
    std::cout << " Operations on a Single Linked List....\n";
    bool shouldExit = false;
    while(!shouldExit) {
-        std::cout << "\n1. At beginning\n2. At Position\n3. At end\n4. Display\n5. Delete the Given Data\n6. Delete the last node\n7. Search data\n8. Exit\n";
+        std::cout << "\n1. Insert At beginning\n2. Insert At Position\n3. Insert At end\n4. Display the list\n5. Delete the Given Data\n6. Delete the last node\n7. Search data\n8. Exit\n";
         std::cout << "Enter your option: ";
         std::string data;
         int choice;
