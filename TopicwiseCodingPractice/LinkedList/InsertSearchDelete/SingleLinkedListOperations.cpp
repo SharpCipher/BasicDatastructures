c140d2efe39982764ddf4d55e030f33fc7e1269c
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-04-04 21:34:19 +0530

    Operations on SLL

--- TopicwiseCodingPractice/LinkedList/InsertSearchDelete/SingleLinkedListOperations.cpp
+++ TopicwiseCodingPractice/LinkedList/InsertSearchDelete/SingleLinkedListOperations.cpp
@@ -98,6 +98,16 @@ class SingleLinkedList {
                 std::cout << data << " Not found in the list\n";
             }
         }
+        void deleteTheFirstNode() {
+            if(head == nullptr) {
+                std::cout << "List Empty. Nothing to delete!\n";
+                return;
+            }
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+
         void deleteTheLastNode() {
             if(head == nullptr) {
                 std::cout << "List Empty. Nothing to delete!\n";
@@ -154,7 +164,9 @@ int main()
    std::cout << " Operations on a Single Linked List....\n";
    bool shouldExit = false;
    while(!shouldExit) {
-        std::cout << "\n1. Insert At beginning\n2. Insert At Position\n3. Insert At end\n4. Display the list\n5. Delete the Given Data\n6. Delete the last node\n7. Search data\n8. Exit\n";
+        std::cout << "\n1. Insert At beginning\n2. Insert At Position\n3. Insert At end\n";
+        std::cout << "4. Display the list\n5. Delete the Given Data\n6. Delete the first node\n";
+        std::cout << "\n7.Delete the last node\n8. Search data\n9. Exit\n";
         std::cout << "Enter your option: ";
         std::string data;
         int choice;
@@ -193,14 +205,17 @@ int main()
                 sll.deleteTheGivenData(input);
                 break;
             case 6 :
-                sll.deleteTheLastNode();
+                sll.deleteTheFirstNode();
                 break;
             case 7 :
+                sll.deleteTheLastNode();
+                break;
+            case 8 :
                 std::cout << "Enter the data to be searched: ";
                 std::getline(std::cin, searchString);
                 sll.searchData(searchString);
                 break;
-            case 8 :
+            case 9 :
                 std::cout << "\nCurrent List Items: \n";
                 sll.displayList();
                 std::cout << "Exiting...\n";
